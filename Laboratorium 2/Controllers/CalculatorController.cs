b491406ec295fc9b34af5541230711c087af7e95
patry <patry@DESKTOP-IRJ5IDS>
2023-10-23 22:18:58 +0200

    Dodaj pliki projektów.

---
+++ Laboratorium 2/Controllers/CalculatorController.cs
@@ -0,0 +1,73 @@
+﻿using Laboratorium_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public enum Operators
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+
+        //public IActionResult Result(Operators op, double? a, double? b)
+        //{
+        //    if (a == null || b == null)
+        //    {
+        //        return View("Error");
+        //    }
+
+        //    ViewBag.op = op;
+        //    ViewBag.a = a;
+        //    ViewBag.b = b;
+
+        //    switch (op)
+        //    {
+        //        case Operators.Unknown:
+        //            return View("Error");
+        //        case Operators.Add:
+        //            ViewBag.result = a + b;
+        //            break;
+        //        case Operators.Sub:
+        //            ViewBag.result = a - b;
+        //            break;
+        //        case Operators.Mul:
+        //            ViewBag.result = a * b;
+        //            break;
+        //        case Operators.Div:
+        //            ViewBag.result = a / b;
+        //            break;
+        //    }
+        //    return View();
+        //}
+
+        [HttpPost]
+        public IActionResult Result([FromForm] Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+
+        //public IActionResult Result(Calculator model)
+        //{
+        //    if (!model.IsValid())
+        //    {
+        //        return View("Error");
+        //    }
+        //    return View(model);
+        //}
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
---
+++ Laboratorium 2/Models/Brith.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_2.Models
+{
+    public class Birth
+    {
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(Name) && BirthDate < DateTime.Now;
+        }
+
+        public int CalculateAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - BirthDate.Year;
+            if (BirthDate.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
